6c1e6101db320b0d4c3a5ad2d9638b1634f7c069
cryptocore <unrealcryptocore@gmail.com>
2024-07-02 14:30:41 +0200

    Export files

--- lib/backend/init.dart
+++ lib/backend/init.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
+import 'dart:math';
 
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -13,28 +15,46 @@ late File output;
 Future initBeforeShowingUI() async {}
 
 Future<bool> initFile(String content, [String delimiter = ";"]) async {
-  getApplicationDocumentsDirectory().then((val) {
-    output = File("${val.path}/output.csv");
-  });
   _delimiter = delimiter;
   final lines = content.split("\n");
   if (lines.isEmpty) {
     return true;
   }
-  _types = lines[0].split(delimiter);
+
+  var types = lines[0].split(delimiter);
+  var data = <List<String>>[];
+  var nameIdx = -1;
+  var classIdx = -1;
   for (int i = 1; i < lines.length; i++) {
     final values = lines[i].split(delimiter);
-    _data.add(values);
+    data.add(values);
   }
-  for (int i = 0; i < _types.length; i++) {
-    if (_types[i] == "Klasse") {
-      _classIdx = i;
-    } else if (_types[i].startsWith("Name")) {
-      _nameIdx = i;
+  for (int i = 0; i < types.length; i++) {
+    if (types[i] == "Klasse") {
+      classIdx = i;
+    } else if (types[i].startsWith("Name")) {
+      nameIdx = i;
     }
   }
-  if (_classIdx < 0 || _nameIdx < 0) {
+  if (classIdx < 0 || nameIdx < 0) {
     return false;
+  }
+  if (_types.isEmpty && _data.isEmpty) {
+    _types = types;
+    _data = data;
+    _nameIdx = nameIdx;
+    _classIdx = classIdx;
+  } else {
+    for (int i = nameIdx; i < _types.length; i++) {
+      if (_types.contains(types[i])) {
+        for (int j = 0; j < min(_data.length, data.length); j++) {
+          _data[i][j] += data[i][j];
+        }
+      } else {
+        _types.add(types[i]);
+        _data.add(data[i]);
+      }
+    }
   }
   return true;
 }
@@ -49,6 +69,13 @@ void saveFile() async {
 
 Future initAfterShowingUI() async {
   //TODO: Add your code here
+  var val = await getApplicationDocumentsDirectory();
+  output = File("${val.path}/output.csv");
+  if (await output.exists()) {
+    var content = await output.readAsString();
+    await initFile(content);
+    //exportFile();
+  }
 }
 
 Student? onCode(int code, String checkType) {
@@ -83,7 +110,7 @@ Student? onCode(int code, String checkType) {
   var b = false;
   if (checksA.isEmpty) {
     b = false;
-  } else if (checksA.last.startsWith(checkType)){
+  } else if (checksA.last.startsWith(checkType)) {
     b = true;
   }
   checks += "$checkType:${now.hour}-${now.minute},";
@@ -91,6 +118,17 @@ Student? onCode(int code, String checkType) {
   saveFile();
   Student student = Student(name, className, b);
   return student;
+}
+
+List<String> getTimeList(String day) {
+  return [];
+}
+
+void exportFile() async {
+  var params = SaveFileDialogParams(
+    sourceFilePath: output.path,
+  );
+  await FlutterFileDialog.saveFile(params: params);
 }
 
 class Student {
